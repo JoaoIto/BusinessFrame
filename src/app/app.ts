06a6f7098635da39ba1f8f0fcb356eb31e583e54
João <joaovictorpfr@gmail.com>
2023-03-21 16:26:07 -0300

    Fix error verification app.t s

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,12 @@ import { NegociacaoController } from './controllers/negociacao-controller.js';
 
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
-form.addEventListener('submit', event => {
+
+if (form) {
+  form.addEventListener('submit', event => {
     event.preventDefault();
     controller.adiciona();
-});
+  });
+} else {
+  console.error('Elemento form não encontrado.');
+}
