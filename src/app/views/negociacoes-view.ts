99117efc695a574250e238b44ebee1ceac5a67a9
João <joaovictorpfr@gmail.com>
2023-04-05 18:12:40 -0300

    Base tailwind

--- src/app/views/negociacoes-view.ts
+++ src/app/views/negociacoes-view.ts
@@ -5,9 +5,21 @@ export class NegociacoesView extends View<Negociacoes>{
 
   protected template(model: Negociacoes): string {
     return `
-        <table class="table table-hover"> 
-            <thead>
-                <tr class="header-table" >
+        <table class="
+        w-full
+        bg-yellowBgFieldset
+        text-greyFontFieldset
+        border-solid
+        border-2
+        border-yellowBorder
+        rounded-xl
+        "> 
+            <thead class="text-xl bg-white border-solid border-2 border-greyFontFieldset p-3">
+                <tr class="
+                header-table
+                w-5/6
+                h-12
+                " >
                     <th>DATA</th>
                     <th>QUANTIDADE</th>
                     <th>VALOR</th>
@@ -17,10 +29,14 @@ export class NegociacoesView extends View<Negociacoes>{
             <tbody>
                 ${model.lista().map(negociacao => {
                   return `
-                    <tr class="body-table">
-                        <td>${this.formater(negociacao.data)}</td>
-                        <td>${negociacao.quantidade}</td>
-                        <td>${negociacao.valor}</td>
+                    <tr class="
+                    body-table
+                    w-5/6
+                    h-8
+                    ">
+                        <td class="text-xl bg-white border-solid border-2 border-greyFontFieldset p-3">${this.formater(negociacao.data)}</td>
+                        <td class="text-xl bg-white border-solid border-2 border-greyFontFieldset p-3">${negociacao.quantidade}</td>
+                        <td class="text-xl bg-white border-solid border-2 border-greyFontFieldset p-3">${negociacao.valor}</td>
                     </tr>    
                   `
                 }).join("")}
