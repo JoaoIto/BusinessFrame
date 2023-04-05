352ee9f2a24dfd001e0b815ab8454babc25de962
João <joaovictorpfr@gmail.com>
2023-04-05 18:58:10 -0300

    Add verification for malwares script in code compile browser

--- src/app/views/view.ts
+++ src/app/views/view.ts
@@ -1,16 +1,25 @@
 export abstract class View<T>{
 
     protected elemento: HTMLElement | null;
+    private escapar: boolean = false;
 
-    constructor(seletor: string){
+    constructor(seletor: string, escapar?: boolean){
         this.elemento = document.querySelector(seletor);
+        if(escapar){
+            this.escapar = escapar;
+        }
     }
 
     public update(model: T): void {
+         let template = this.template(model);
         if (this.elemento) {
-            const template = this.template(model);
             this.elemento.innerHTML = template;
         }
+        if(this.escapar){
+            template = template
+            .replace(/<script>[\s\S]*?<\/script>/, '')
+        }
+        return
     }
 
     protected abstract template(model: T): string;
